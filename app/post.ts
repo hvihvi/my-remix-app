80ebc913f42dfd658c9bdcc4601869dde5e6e156
sja <sirine.jabari@lesfurets.com>
2021-12-13 17:45:33 +0100

    create form for new post

--- app/post.ts
+++ app/post.ts
@@ -9,6 +9,12 @@ export type Post = {
     title: string;
 };
 
+type NewPost = {
+    title: string;
+    slug: string;
+    markdown: string;
+};
+
 export type PostMarkdownAttributes = {
     title: string;
 };
@@ -54,4 +60,14 @@ export async function getPost(slug: string) {
     );
     const html = marked(body);
     return { slug, html, title: attributes.title };
+}
+
+// ...
+export async function createPost(post: NewPost) {
+    const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+    await fs.writeFile(
+        path.join(postsPath, post.slug + ".md"),
+        md
+    );
+    return getPost(post.slug);
 }
---
+++ app/routes/admin.tsx
@@ -0,0 +1,35 @@
+import {Link, Outlet, useLoaderData} from "remix";
+import type {Post} from "~/post";
+import {getPosts} from "~/post";
+import adminStyles from "~/styles/admin.css";
+
+export const links = () => {
+    return [{rel: "stylesheet", href: adminStyles}];
+};
+
+export const loader = () => {
+    return getPosts();
+};
+
+export default function Admin() {
+    const posts = useLoaderData<Post[]>();
+    return (
+        <div className="admin">
+            <nav>
+                <h1>Admin</h1>
+                <ul>
+                    {posts.map(post => (
+                        <li key={post.slug}>
+                            <Link to={`/posts/${post.slug}`}>
+                                {post.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    );
+}
